8e37f43e2064e1ed473c01065f1152e6d7bc20bd
Arpad Borsos <arpad.borsos@googlemail.com>
2014-02-24 11:57:38 +0100

    play with a different code coverage setup

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 /* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
 "use strict";
 
-module.exports = process.env.MODEL_CAST_COV
-	? require('./lib-cov')
-	: require('./lib');
+var exports = module.exports = bindCast;
+
+// convenience method because `Date` does not work when called as function
+exports.Date = function (d) {
+	if (d instanceof Date)
+		return d;
+	return new Date(d);
+};
+
+// convenience method that works well with Swatinem/model-cache
+exports.Cache = function (Model) {
+	return function (key) {
+		if (key instanceof Model)
+			return key;
+		return Model.get(key);
+	};
+};
+
+function bindCast(Model) {
+	Model.cast = defineCast;
+}
+
+function defineCast(name, fn) {
+	/* jshint validthis: true */
+
+	var desc = Object.getOwnPropertyDescriptor(this.prototype, name);
+	var setter = desc.set;
+	desc.set = function (val) {
+		setter.call(this, fn(val));
+	};
+	Object.defineProperty(this.prototype, name, desc);
+
+	return this;
+}
+
